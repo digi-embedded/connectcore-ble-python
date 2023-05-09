449c92c2fa638c6138532746056e4bc05e2d4abe
David Escalona <david.escalona@digi.com>
2023-05-09 09:38:13 +0200

    setup: add 'wheel' package to setup requirements
    Signed-off-by: David Escalona <david.escalona@digi.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2022, Digi International Inc.
+# Copyright 2022, 2023, Digi International Inc.
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -79,4 +79,5 @@ setup(
         'Source': 'https://github.com/digi-embedded/connectcore-ble-python',
         'Tracker': 'https://github.com/digi-embedded/connectcore-ble-python/issues',
     },
+    setup_requires=['wheel']
 )
